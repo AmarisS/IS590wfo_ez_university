ddb1062b8b02f133b8772caa6da4c55efd2d4c75
AmarisS <sixuans2@illinois.edu>
2018-12-12 01:02:00 -0600

    Deployment Assignment

---
+++ courseinfo/admin.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from .models import Period, Year, Semester, Section, Course, Instructor, Student, Registration
+
+admin.site.register(Period)
+admin.site.register(Year)
+admin.site.register(Semester)
+admin.site.register(Section)
+admin.site.register(Course)
+admin.site.register(Instructor)
+admin.site.register(Student)
+admin.site.register(Registration)
